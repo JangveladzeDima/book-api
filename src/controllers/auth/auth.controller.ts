b3e9c4c85a2d893a5772981f7f2b47ef33d6492c
djangveladze <dima.jangveladze@ozorix.com>
2024-01-20 03:51:23 +0400

    added swagger

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,11 +2,15 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "../../core/services/auth.service";
 import { SignUpDto } from "./dto/sign-up.dto";
 import { LoginDto } from "./dto/login.dto";
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse } from "@nestjs/swagger";
 
 @Controller("/auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {
+  }
 
+  @ApiCreatedResponse({ description: "Register New User" })
+  @ApiBadRequestResponse({ description: "Email Already Exists Or Data Validation Error" })
   @Post("/sign-up")
   async signUp(@Body() data: SignUpDto) {
     await this.authService.signUp(data);
@@ -16,6 +20,9 @@ export class AuthController {
     };
   }
 
+  @ApiCreatedResponse({ description: "Created Access Token" })
+  @ApiNotFoundResponse({ description: "User With Credentials Not Found" })
+  @ApiBadRequestResponse({ description: "Data Validation Error" })
   @Post("/login")
   async login(@Body() data: LoginDto) {
     return this.authService.login(data);
