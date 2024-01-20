b1e782bdf40201f98acf728524553ab41f45f41f
djangveladze <dima.jangveladze@ozorix.com>
2024-01-20 17:49:38 +0400

    added swagger descriptions

--- src/controllers/book/book.controller.ts
+++ src/controllers/book/book.controller.ts
@@ -12,11 +12,17 @@ import {
 import { CreateBookDto } from "./dto/create-book.dto";
 import { BookService } from "../../core/services/book.service";
 import { UpdateLastReadPageDto } from "./dto/update-last-read-page.dto";
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from "@nestjs/swagger";
 
 @Controller("/book")
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  @ApiCreatedResponse({ description: "Create Book And Book Pages" })
   @Post()
   async createBook(@Req() request: Request, @Body() data: CreateBookDto) {
     return this.bookService.createBook(
@@ -26,11 +32,14 @@ export class BookController {
     );
   }
 
+  @ApiOkResponse({ description: "Return Book By Id" })
+  @ApiNotFoundResponse({ description: "Return Then Book With Id Not Found" })
   @Get("/:id")
   async fetch(@Param("id") id: number) {
     return this.bookService.fetch(id);
   }
 
+  @ApiOkResponse({ description: "Return Books By PageIndex and PageSize" })
   @Get()
   async load(
     @Query("pageIndex") pageIndex?: string,
@@ -39,6 +48,10 @@ export class BookController {
     return this.bookService.load(parseInt(pageIndex), parseInt(pageSize));
   }
 
+  @ApiOkResponse({
+    description: "Return Then Set Last Read Page Finish Successful",
+  })
+  @ApiNotFoundResponse({ description: "Return Then Not Found Book Or Page" })
   @Put("/:bookId/last-read-page")
   async updateLastReadPage(
     @Param("bookId") bookId: number,
@@ -47,6 +60,8 @@ export class BookController {
     return this.bookService.updatePageLastReadBook(bookId, data.pageId);
   }
 
+  @ApiOkResponse({ description: "Return Then Book Delete Finish Successful" })
+  @ApiNotFoundResponse({ description: "Return Then Not Found Book" })
   @Delete("/:id")
   async deleteBook(@Param("id") id: number) {
     return this.bookService.delete(id);
